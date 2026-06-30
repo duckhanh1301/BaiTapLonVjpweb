d77ae357ba29318948371bc40df1482ecd27eb62
Khanh Hung <khanhkk173@gmail.com>
2026-06-30 23:07:22 +0700

    Thêm Components header

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+const path = require("path");
 const db = require("./config/db");
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 app.get("/", (req, res) => {
 
     res.send("Backend quản lý tòa nhà đang chạy");
